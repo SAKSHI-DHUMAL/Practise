3410c221aee207f8f1b9bf5c9a45e9e4d718d59e
SAKSHI-DHUMAL <141111195+SAKSHI-DHUMAL@users.noreply.github.com>
2025-07-12 17:24:18 +0530

    Update File.js

--- File.js
+++ File.js
@@ -5,4 +5,4 @@ console.log(` Multiplication Of 2 Numbers is: ${a * b}`);
 console.log("Hello, Vighnesh");
 console.log("Hello, Sakshi");
 
-console.log("Welcome to the IT Industy");
+console.log("Welcome to the IT Indusrty");
