e4c9051902d784ee2c443bf2b8408f76522394d5
SAKSHI-DHUMAL <141111195+SAKSHI-DHUMAL@users.noreply.github.com>
2025-07-12 18:00:59 +0530

    Update File.js

--- File.js
+++ File.js
@@ -6,3 +6,7 @@ console.log("Hello Vighnesh");
 console.log("Hello, Sakshi");
 
 console.log("Welcome to the IT Indusrty");
+
+const arr=[1,2,3,4];
+const sum=arr.reduce((acc, curr) =>return acc+curr;)};
+console.log(sum);
