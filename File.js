884594e8be351531707c7ee985932c7aef0ad477
Sakshi23 <sakshi123@gmail.com>
2025-07-14 11:58:24 +0530

    add a javascript closure code

--- File.js
+++ File.js
@@ -1,21 +1,40 @@
-let a = 10;
-let b = 5;
-console.log(` Multiplication Of 2 Numbers is: ${a * b}`);
+// let a = 10;
+// let b = 5;
+// console.log(` Multiplication Of 2 Numbers is: ${a * b}`);
 
-console.log("Hello Vighnesh");
-console.log("Hello, Sakshi");
+// console.log("Hello Vighnesh");
+// console.log("Hello, Sakshi");
 
-console.log("Welcome to the IT Indusrty");
+// console.log("Welcome to the IT Indusrty");
 
-const arr = [1, 2, 3, 4];
-const sum = arr.reduce((acc, curr) => acc + curr);
-console.log(sum);
+// const arr = [1, 2, 3, 4];
+// const sum = arr.reduce((acc, curr) => acc + curr);
+// console.log(sum);
 
-const nums = [2, 4, 3, 5, 1, 6];
-const evens = nums.filter((nums) => nums % 2 === 0);
-console.log(evens);
+// const nums = [2, 4, 3, 5, 1, 6];
+// const evens = nums.filter((nums) => nums % 2 === 0);
+// console.log(evens);
 
+// const names = ["Alice", "Bob", "Charlie", "Denver"];
+// const capitalized = names.map((names) => CharAt(0).toUpperCase().slice(1));
+// console.log(capitalized);
 
-const names = ["Alice", "Bob", "Charlie", "Denver"];
-const capitalized = names.map((names) => CharAt(0).toUpperCase().slice(1));
-console.log(capitalized);
+function outer() {
+  let counter = 0;
+
+  function inner() {
+    counter++;
+    console.log(counter);
+  }
+  return inner;
+}
+
+let myFun = outer();
+
+myFun();
+
+myFun();
+
+myFun();
+
+myFun();
